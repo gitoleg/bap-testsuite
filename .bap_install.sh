96540234d967c8530a7d1108a43bbf934b78078a
oleg <forown@yandex.ru>
2016-11-10 11:59:16 -0500

    modified:   .bap_install.sh

--- .bap_install.sh
+++ .bap_install.sh
@@ -3,5 +3,7 @@
 BAP_BRANCH=fix-llvm-segfault-PR
 
 bash -ex .travis-opam.sh
-opam pin add bap  https://github.com/BinaryAnalysisPlatform/bap#$BAP_BRANCH
+
+opam depext bap
+opam pin add bap https://github.com/BinaryAnalysisPlatform/bap#$BAP_BRANCH
 opam install bap
