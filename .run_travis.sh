e4883cca4da593d3dd6d7c7d2e76e4fa2b107d3f
oleg <forown@yandex.ru>
2016-11-11 11:57:09 -0500

    modified:   .run_travis.sh

--- .run_travis.sh
+++ .run_travis.sh
@@ -2,8 +2,8 @@
 
 #bash -ex .travis-opam.sh
 echo "branch: "
-echo $TRAVIS_BRANCH
 echo $TRAVIS_PULL_REQUEST_BRANCH
+echo $TRAVIS_REPO_SLUG
 #opam pin add bap https://github.com/BinaryAnalysisPlatform/bap#$BAP_BRANCH
 
 
