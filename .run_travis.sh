5d2a8bbf9c32239653a6f7876db9b554aa08702f
oleg <forown@yandex.ru>
2016-11-11 15:52:58 -0500

    modified:   .run_travis.sh

--- .run_travis.sh
+++ .run_travis.sh
@@ -4,7 +4,9 @@ REPO=$TRAVIS_REPO_SLUG
 USER=${REPO%/*}
 BAP_REPO=https://github.com/$USER/bap
 
+make check
+
 bash -ex .travis-opam.sh
 opam pin add bap $BAP_REPO#$TRAVIS_PULL_REQUEST_BRANCH
 
-make check
+
