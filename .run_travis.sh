7c3fa6096f4932a8130d27408f592ff68cf33e53
oleg <forown@yandex.ru>
2016-11-16 16:10:27 -0500

    modified:   .run_travis.sh

--- .run_travis.sh
+++ .run_travis.sh
@@ -7,12 +7,10 @@ BAPDIR=bap-source
 PR_NUM=${TRAVIS_PULL_REQUEST_BRANCH##*#}
 BRANCH=+refs/pull/$PR_NUM/merge
 
-git clone --depth=50 https://github.com/BinaryAnalysisPlatform/bap.git $BAPDIR
+git clone --depth=50 https://github.com/gitoleg/bap.git $BAPDIR
 cd $BAPDIR
-git fetch origin $BRANCH
-git checkout -qf FETCH_HEAD
 git checkout -b run-travis
-opam pin -yn add bap git+file://$PWD#run-travis
+opam pin -yn add bap $PWD
 opam install bap --deps-only
 opam install bap -v
 
