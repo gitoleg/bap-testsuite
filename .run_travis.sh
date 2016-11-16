5c64fc0d81956b2b0df6bd0c08b8583dd1b3f6ea
oleg <forown@yandex.ru>
2016-11-16 08:51:28 -0500

    modified:   .run_travis.sh

--- .run_travis.sh
+++ .run_travis.sh
@@ -4,19 +4,19 @@ bash -ex .travis-opam.sh
 eval `opam config env`
 opam install bap --deps-only
 
-
+BAPDIR=bap-source
 NUMBER=${TRAVIS_PULL_REQUEST_BRANCH##*#}
 BRANCH=+refs/pull/$NUMBER/merge
 
-git clone --depth=50 https://github.com/BinaryAnalysisPlatform/bap.git BinaryAnalysisPlatform/bap
-cd BinaryAnalysisPlatform/bap
+git clone --depth=50 https://github.com/BinaryAnalysisPlatform/bap.git $BAPDIR
+cd $BAPDIR
 git fetch origin $BRANCH
 git checkout -qf FETCH_HEAD
 git checkout -b run-travis
 cat plugins/llvm/llvm_binary_34.hpp
-opam pin -yn add bap file://$PWD
+opam pin -yn add bap $PWD
 opam install bap --deps-only
 opam install bap -v
 
-cd ../..
+cd ..
 make check
