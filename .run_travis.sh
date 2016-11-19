9efa066b8e0f68961b0282fe169dc55e40a9ac3d
oleg <forown@yandex.ru>
2016-11-19 17:51:28 -0500

    modified:   .run_travis.sh

--- .run_travis.sh
+++ .run_travis.sh
@@ -8,11 +8,10 @@ USER=${TRAVIS_REPO_SLUG%/*}
 PR_NUM=${TRAVIS_PULL_REQUEST_BRANCH##*#}
 BRANCH=+refs/pull/$PR_NUM/merge
 
-#git clone --depth=50 https://github.com/$USER/bap.git $BAPDIR
-git clone --depth=50 https://github.com/gitoleg/bap $BAPDIR
+git clone --depth=50 https://github.com/$USER/bap.git $BAPDIR
 cd $BAPDIR
-#git fetch origin $BRANCH
-#git checkout -qf FETCH_HEAD
+git fetch origin $BRANCH
+git checkout -qf FETCH_HEAD
 opam pin -yn add bap .
 opam install bap --deps-only
 opam install bap -v
